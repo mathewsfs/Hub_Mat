267ea6e48727b54781f147d27a14ea8a5d888cf6
MaTTws <83310944+mathewsfs@users.noreply.github.com>
2024-08-06 21:46:33 -0300

    Add files via upload

---
+++ Karoline.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parabéns Karoline</title>
+    <style>
+        body {
+            background-image: url('https://github.com/mathewsfs/Hub_Mat/blob/0b6e445c7e2666b657667d4e4a0d4dc4a98a6ffb/delicious-donuts-with-topping-arrangement.jpg?raw=true');
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: 'Arial', sans-serif;
+        }
+
+        .container {
+            text-align: center;
+            background: rgba(255, 255, 255, 0.9); /* Fundo branco translúcido */
+            padding: 20px;
+            border: 5px solid #ffb347;
+            border-radius: 15px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            max-width: 90%;
+            margin: 10px;
+        }
+
+        .container h1 {
+            color: #ff6347;
+            font-size: 2em;
+            margin-bottom: 20px;
+        }
+
+        .container p {
+            font-size: 1em;
+            color: #555;
+            line-height: 1.6;
+        }
+
+        .container img {
+            width: 80px;
+            margin-top: 20px;
+        }
+
+        @media (min-width: 600px) {
+            .container {
+                max-width: 600px;
+                padding: 50px;
+                border: 10px solid #ffb347;
+            }
+
+            .container h1 {
+                font-size: 2.5em;
+            }
+
+            .container p {
+                font-size: 1.2em;
+            }
+
+            .container img {
+                width: 100px;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Parabéns Karoline!</h1>
+        <p>Por ter vendido todos os Donuts, Eu sei que você vai fazer muito sucesso, e que em breve vai conseguir fazer a sua lojinha, sei que não vai ser fácil, mas você vai conseguir, você é capaz.</p>
+        
+    </div>
+</body>
+</html>
